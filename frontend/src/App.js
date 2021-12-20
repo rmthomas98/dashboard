742216a6de26ea79d1e8c25657cf212b3f188bc0
rmthomas98 <rmthomas1998@gmail.com>
2021-12-19 21:29:01 -0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import DashboardContainer from "./DashboardContainer";
+
+const App = () => {
+  return (
+    <div className="App font-sans bg-gradient-to-b from-slate-700 to-slate-900 min-h-screen flex items-center">
+      <DashboardContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/DashboardContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardContainer = () => {
+  return <div className="border border-slate-500 mx-auto w-full h-full">b</div>;
+};
+
+export default DashboardContainer;
